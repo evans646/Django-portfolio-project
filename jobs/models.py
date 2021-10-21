6ed6eb36b7ee0a17e1923b2097565413b64069a1
Evans <jw207740@gmail.com>
2021-10-21 20:21:41 +0000

    changes

--- jobs/models.py
+++ jobs/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    description = models.CharField(max_length=300,default="project description")
  #function to return the summary so inside the db we will get a summary of the project instead of just object1 and so on 
     def __str__(self):
         return self.summary
