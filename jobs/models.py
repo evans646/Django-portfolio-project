435ebaf585e1f7604ef7411e55061235b4dcc74f
Evans <jw207740@gmail.com>
2021-10-21 13:45:47 +0000

    init  commit

---
+++ jobs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+ #function to return the summary so inside the db we will get a summary of the project instead of just object1 and so on 
+    def __str__(self):
+        return self.summary
