347513f09c315775d80e7a764948cad82808366b
Evans <jw207740@gmail.com>
2021-10-22 13:13:57 +0000

    changes

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
-    description = models.CharField(max_length=300,default="project description")
- #function to return the summary so inside the db we will get a summary of the project instead of just object1 and so on 
+    description = models.CharField(max_length=2000,default="project description")
+    url = models.CharField(max_length=200,default="project url")
+ #function to return the summary so inside the db we will get a summary of the project instead of just: object1 and so on .....
     def __str__(self):
         return self.summary
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.index,name='index'),
     path('jobs/<int:job_id>', jobs.views.detail, name='detail'),
-] 
+]
 
 urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
