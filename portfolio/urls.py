98847d23f95572dba8c393ad9bf637574c14d3ee
Evans <jw207740@gmail.com>
2021-10-23 10:46:50 +0000

    changes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls,),
     path('', jobs.views.index, name='index'),
-    path('jobs/<int:job_id>',jobs.views.detail,include('portfolio.urls')),
+    path('jobs/<int:job_id>',jobs.views.detail,name='detail'),
     # path('https://portfolio656.herokuapp.com/',include('portfolio.urls')),
 ]
 
