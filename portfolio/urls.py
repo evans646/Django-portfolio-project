78f198480f0eb2fb1d257dc7389c2991aa0ca262
Evans <jw207740@gmail.com>
2021-10-23 10:33:41 +0000

    changes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,20 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import jobs.views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls,),
-    path('', jobs.views.index,include('portfolio.urls')),
-    path('jobs/<int:job_id>', jobs.views.detail,include('portfolio.urls')),
+    path('', jobs.views.index, name='index'),
+    path('jobs/<int:job_id>', jobs.views.detail,name='detail'),
+    path('',jobs.views.detail,include('portfolio.urls')),
 ]
 
 
 urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 # urlpatterns+=include('portfolio.urls')
+
 
