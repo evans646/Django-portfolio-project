f0dcb61803a33c56a1d7c32e1a67eb791ac6085a
Evans <jw207740@gmail.com>
2021-11-07 15:27:20 +0000

    changes

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -1,11 +1,17 @@
+"""
+WSGI config for portfolio project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
 import os
+
 from whitenoise import WhiteNoise
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
-
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(get_wsgi_application())
+application = WhiteNoise(application)
 
